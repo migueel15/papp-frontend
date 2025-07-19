27fb040e6619eec836a3b5e0528a2f8c17c5c547
migueel15 <miguelangeldorado10@gmail.com>
2025-07-19 03:50:55 +0200

    login

--- src/features/auth/LoginPage.tsx
+++ src/features/auth/LoginPage.tsx
@@ -7,17 +7,36 @@ const LoginPage = () => {
 
   return (
     <div className="flex justify-center items-center w-screen h-screen bg-bg-dark">
-      <div className="flex flex-col items-center text-center rounded-2xl w-100 h-100 bg-bg">
-        <img src={icon} alt="papp icon" width={60} className="mt-10" />
-        <h1 className="font-poppins-semibold text-xl text-text mt-5 mb-10">
-          Log In to your account
-        </h1>
+      <div className="flex flex-col items-center rounded-2xl w-100 px-10 bg-bg shadow-xl">
+        <header className="flex flex-col items-center mt-10 mb-auto">
+          <img src={icon} alt="papp icon" width={60} />
+          <h1 className="font-poppins-semibold text-xl text-text mt-5 mb-10">
+            Log In to your account
+          </h1>
+        </header>
+        <div className="flex flex-col items-center mb-auto gap-2">
+          <input
+            type="email"
+            className="bg-bg-light rounded-md px-3 h-10 py-2 text-text shadow-sm"
+            disabled={true}
+            placeholder="Email"
+          />
+          <input
+            type="password"
+            className="bg-bg-light rounded-md px-3 h-10 py-2 text-text shadow-sm"
+            disabled={true}
+            placeholder="Password"
+          />
+          <button className="bg-bg-light h-10 shadow-sm w-full rounded-md text-text cursor-pointer hover:bg-highlight hover:text-bg-dark transition duration-200">
+            Log in
+          </button>
+        </div>
         <button
-          className="bg-bg-light h-12 rounded-2xl text-bg mb-5 flex items-center cursor-pointer"
+          className="flex justify-center gap-2 shadow-sm bg-bg-light h-12 rounded-md mb-10 mt-5 w-full items-center cursor-pointer px-5 hover:bg-highlight text-text hover:text-bg-dark transition duration-200"
           onClick={() => auth.login()}
         >
-          <img src={google} alt="" width={20} className="mx-2" />
-          <p className="mr-2 text-text">Login with Google</p>
+          <img src={google} alt="" width={20} className="text-blue-500" />
+          <p>Login with Google</p>
         </button>
       </div>
     </div>
