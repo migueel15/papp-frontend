50e77a6398347c02f927e66c0356d13c6d8d12a3
migueel15 <miguelangeldorado10@gmail.com>
2025-07-19 04:14:36 +0200

    fix responsive

--- src/features/auth/LoginPage.tsx
+++ src/features/auth/LoginPage.tsx
@@ -7,14 +7,14 @@ const LoginPage = () => {
 
   return (
     <div className="flex justify-center items-center w-screen h-screen bg-bg-dark">
-      <div className="flex flex-col items-center rounded-2xl w-100 px-10 bg-bg shadow-xl">
+      <div className="flex flex-col items-center rounded-2xl w-[90%] sm:w-100 px-10 bg-bg shadow-xl">
         <header className="flex flex-col items-center mt-10 mb-auto">
           <img src={icon} alt="papp icon" width={60} />
-          <h1 className="font-poppins-semibold text-xl text-text mt-5 mb-10">
+          <h1 className="font-poppins-semibold text-xl text-text mt-5 mb-10 text-center">
             Log In to your account
           </h1>
         </header>
-        <div className="flex flex-col items-center mb-auto gap-2">
+        <div className="flex flex-col mb-auto gap-2 w-full">
           <input
             type="email"
             className="bg-bg-light rounded-md px-3 h-10 py-2 text-text shadow-sm"
