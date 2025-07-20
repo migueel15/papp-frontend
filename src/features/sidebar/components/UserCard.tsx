b2777d2b11df544db8566cde7cd115639ab2f9e5
migueel15 <miguelangeldorado10@gmail.com>
2025-07-20 17:15:31 +0200

    show info or login

--- src/features/sidebar/components/UserCard.tsx
+++ src/features/sidebar/components/UserCard.tsx
@@ -2,22 +2,31 @@ import { useAuth } from "@/features/auth/auth.hook";
 
 const UserCard = () => {
   const auth = useAuth();
+  const isAuthenticated = auth.isAuthenticated;
 
   return (
-    <div className="flex bg-bg-light p-2 rounded-lg items-center gap-2 drop-shadow-md">
-      <img
-        src={auth.user?.picture}
-        alt="icon"
-        className="aspect-square w-7 h-7 rounded-full"
-      />
-      <div className="flex flex-col truncate">
-        <p className="text-xs text-nowrap text-text truncate">
-          {auth.user?.name}
-        </p>
-        <p className="text-[10px] text-nowrap text-text-muted truncate">
-          {auth.user?.email}
-        </p>
-      </div>
+    <div className="flex bg-bg-light p-2 h-12 rounded-lg items-center gap-2 drop-shadow-md">
+      {isAuthenticated ? (
+        <>
+          <img
+            src={auth.user?.picture}
+            alt="icon"
+            className="aspect-square w-7 h-7 rounded-full"
+          />
+          <div className="flex flex-col truncate">
+            <p className="text-xs text-nowrap text-text truncate">
+              {auth.user?.name}
+            </p>
+            <p className="text-[10px] text-nowrap text-text-muted truncate">
+              {auth.user?.email}
+            </p>
+          </div>
+        </>
+      ) : (
+        <div className="flex flex-1 justify-center">
+          <p className="text-xs text-text-muted">Please log in</p>
+        </div>
+      )}
     </div>
   );
 };
